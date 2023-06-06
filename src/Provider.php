7830d8b9d901a56e97345c477de2fbfe1f1cb5cd
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2023-06-06 22:43:49 +0300

    first commit

---
+++ src/Provider.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace MultipleChain\Solana;
+
+use Exception;
+
+final class Provider
+{
+    /**
+     * @var array
+     */
+    private $chainIds = [
+        "devnet" => 103,
+        "testnet" => 102,
+        "mainnet-beta" => 101,
+    ];
+    
+    /**
+     * @var array
+     */
+    private $networks = [
+        "mainnet" => [
+            "node" => "mainnet-beta",
+            "name" => "Mainnet",
+            "host" => "https://api.mainnet-beta.solana.com/",
+            "explorer" => "https://solscan.io/"
+        ],
+        "testnet" => [
+            "node" => "testnet",
+            "name" => "Testnet",
+            "host" => "https://api.testnet.solana.com",
+            "explorer" => "https://solscan.io/"
+        ],
+        "devnet" => [
+            "node" => "devnet",
+            "name" => "Devnet",
+            "host" => "https://api.devnet.solana.com",
+            "explorer" => "https://solscan.io/"
+        ]
+    ];
+
+    /**
+     * @var array
+     */
+    private $errorCodes = [
+        "parse-error" => -32700,
+        "invalid-request" => -32600,
+        "method-not-found" => -32601,
+        "invalid-parameters" => -32602,
+        "internal-error" => -32603,
+    ];
+
+    /**
+     * @var array
+     */
+    private $allowedMethods = [
+        'getAccountInfo', 'getBalance', 'getBlock', 'getBlockHeight', 'getBlockProduction', 'getBlockCommitment', 'getBlocks', 'getBlocksWithLimit', 'getBlockTime', 'getClusterNodes', 'getEpochInfo', 'getEpochSchedule', 'getFeeForMessage', 'getFirstAvailableBlock', 'getGenesisHash', 'getHealth', 'getHighestSnapshotSlot', 'getIdentity', 'getInflationGovernor', 'getInflationRate', 'getInflationReward', 'getLargestAccounts', 'getLatestBlockhash', 'getLeaderSchedule', 'getMaxRetransmitSlot', 'getMaxShredInsertSlot', 'getMinimumBalanceForRentExemption', 'getMultipleAccounts', 'getProgramAccounts', 'getRecentPerformanceSamples', 'getSignaturesForAddress', 'getSignatureStatuses', 'getSlot', 'getSlotLeader', 'getSlotLeaders', 'getStakeActivation', 'getSupply', 'getTokenAccountBalance', 'getTokenAccountsByDelegate', 'getTokenAccountsByOwner', 'getTokenLargestAccounts', 'getTokenSupply', 'getTransaction', 'getTransactionCount', 'getVersion', 'getVoteAccounts', 'isBlockhashValid', 'minimumLedgerSlot', 'requestAirdrop', 'sendTransaction', 'simulateTransaction', 'accountSubscribe', 'accountUnsubscribe', 'logsSubscribe', 'logsUnsubscribe', 'programSubscribe', 'programUnsubscribe', 'signatureSubscribe', 'signatureUnsubscribe', 'slotSubscribe', 'slotUnsubscribe'
+    ];
+
+    /**
+     * @var object
+     */
+    public $network;
+
+    /**
+     * @var int
+     */
+    private $randomKey;
+
+    /**
+     * @param bool $testnet
+     * @param string|null $customRpc
+     * @throws Exception
+     */
+    public function __construct(bool $testnet = false, string $customRpc = null) 
+    {
+        $this->network = (object) $this->networks[$testnet ? 'devnet' : 'mainnet'];
+        if (!$testnet && $customRpc) {
+            $this->network->host = $customRpc;
+        }
+
+        $this->randomKey = random_int(0, 99999999);
+    }
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @return mixed
+     * @throws Exception
+     */
+    public function __call(string $method, array $params = [])
+    {
+        if (preg_match('/^[a-zA-Z0-9]+$/', $method) === 1) {
+
+            if (!in_array($method, $this->allowedMethods)) {
+                throw new Exception('Unallowed method: ' . $method);
+            }
+            
+            return $this->call($method, ...$params);
+        } else {
+            throw new Exception('Invalid method name');
+        }
+    }
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @return mixed
+     * @throws Exception
+     */
+    public function call(string $method, ...$params)
+    {
+        $curl = curl_init($this->network->host);
+
+        $headers = [
+            "Content-Type: application/json"
+        ];
+
+        $rpc = $this->buildRpc($method, $params);
+
+        curl_setopt_array($curl, [
+            CURLOPT_POST => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_POSTFIELDS => $rpc
+        ]);
+
+        $response = json_decode(curl_exec($curl));
+
+        curl_close($curl);
+
+        $this->validateResponse($response, $method, $params);
+
+        return $response ? $response->result : null;
+    }
+
+    /**
+     * @param object $response
+     * @param string $method
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    protected function validateResponse(object $response, string $method, array $params) : void
+    {
+        if ($response->id !== $this->randomKey) {
+            throw new Exception('Invalid response');
+        }
+
+        if (isset($response->error)) {
+            if ($response->error->code === $this->errorCodes['method-not-found']) {
+                throw new Exception("API Error: Method {$method} not found.");
+            } else {
+                throw new Exception($response->error->message);
+            }
+        }
+    }
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @return string
+     */
+    public function buildRpc(string $method, array $params) : string
+    {
+        return json_encode([
+            'jsonrpc' => '2.0',
+            'id' => $this->randomKey,
+            'method' => $method,
+            'params' => $params,
+        ]);
+    }
+    
+    /**
+     * @return int
+     */
+    public function getRandomKey() : int
+    {
+        return $this->randomKey;
+    }
+    
+
+    /**
+     * @param string $hash
+     * @return Transaction
+     */
+    public function Transaction(string $hash) : Transaction
+    {
+        return new Transaction($hash, $this);
+    }
+}
---
+++ src/Transaction.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace MultipleChain\Solana;
+
+use Exception;
+use MultipleChain\Utils;
+
+final class Transaction
+{
+    /**
+     * @var Provider
+     */
+    private $provider;
+    
+    /**
+     * Transaction hash
+     * @var string
+     */
+    private $hash;
+
+    /**
+     * Transaction data
+     * @var object
+     */
+    private $data;
+
+    /**
+     * @param string $hash
+     * @param Provider $provider
+     * @throws Exception
+     */
+    public function __construct(string $hash, Provider $provider)
+    {
+        $this->hash = $hash;
+        $this->provider = $provider;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHash() : string
+    {
+        return $this->hash;
+    }
+
+    /**
+     * @return object|null
+     */
+    public function getData() : ?object
+    {
+        return $this->data = $this->provider->getTransaction($this->hash);
+    }
+
+    /**
+     * @return ?bool
+     */
+    public function getStatus() : ?bool
+    {
+        $this->getData();
+        $result = null;
+
+        if ($this->data == null) {
+            $result = false;
+        } else {
+            if (is_null($this->data->meta->err)) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return bool
+     */
+    public function validate() : bool
+    {
+        $result = $this->getStatus();
+        
+        if (is_bool($result)) {
+            return $result;
+        } else {
+            return $this->validate();
+        }
+    }
+
+    /**
+     * @param int amount 
+     * @return bool
+     */
+    public function verifyTokenTransferWithData(float $amount) : bool
+    {
+        if ($this->validate()) {
+            $decimals = $this->data->meta->preTokenBalances[0]->uiTokenAmount->decimals;
+            $beforeBalance = $this->data->meta->preTokenBalances[0]->uiTokenAmount->uiAmount;
+            $afterBalance = $this->data->meta->postTokenBalances[0]->uiTokenAmount->uiAmount;
+            $diff = ($beforeBalance - $afterBalance);
+
+            if (!isset($this->data->meta->preTokenBalances[1])) {
+                $beforeBalance = $this->data->meta->preTokenBalances[0]->uiTokenAmount->uiAmount;
+                $afterBalance = $this->data->meta->postTokenBalances[1]->uiTokenAmount->uiAmount;
+                $diff = ($beforeBalance - $afterBalance);
+            } elseif (!($diff == $amount)) {
+                $decimals = $this->data->meta->preTokenBalances[1]->uiTokenAmount->decimals;
+                $beforeBalance = $this->data->meta->preTokenBalances[1]->uiTokenAmount->uiAmount;
+                $afterBalance = $this->data->meta->postTokenBalances[1]->uiTokenAmount->uiAmount;
+                $diff = ($beforeBalance - $afterBalance);
+            }
+            
+            if (!($diff == $amount)) {
+                $diff = floatval(number_format(abs($beforeBalance - $afterBalance), $decimals, '.', ""));
+            }
+
+            if (!($diff == $amount)) {
+                $amount = floatval(number_format($amount, $decimals, '.', ""));
+            }
+
+            return Utils::toString($diff, $decimals) == Utils::toString($amount, $decimals);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param int amount 
+     * @return bool
+     */
+    public function verifyCoinTransferWithData(float $amount) : bool 
+    {
+        if ($this->validate()) {
+            $beforeBalance = $this->data->meta->preBalances[0];
+            $afterBalance = $this->data->meta->postBalances[0];
+            $diff = Utils::toDec(($afterBalance - $beforeBalance), 9);
+            
+            if (!($diff == $amount)) {
+                $beforeBalance = $this->data->meta->preBalances[1];
+                $afterBalance = $this->data->meta->postBalances[1];
+                $diff = Utils::toDec(($afterBalance - $beforeBalance), 9);
+            }
+
+            return Utils::toString($diff, 9) == Utils::toString($amount, 9);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param object $config
+     * @return bool
+     */
+    public function verifyTransferWithData(object $config) : bool
+    {
+        if (isset($config->tokenAddress) && !is_null($config->tokenAddress)) {
+            return $this->verifyTokenTransferWithData($config->amount);
+        } else {
+            return $this->verifyCoinTransferWithData($config->amount);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl() 
+    {
+        $node = $this->provider->network->node;
+        $url  = $this->provider->network->explorer . "tx/" . $this->hash;
+        $url .= $node != 'mainnet-beta' ? '?cluster=' . $node : '';
+        return $url;
+    }
+}
